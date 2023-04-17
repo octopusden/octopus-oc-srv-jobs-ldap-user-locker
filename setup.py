85b1ffaf4af39ddd515270a3869e1493cb276462
ivansa-ru <ivansa@me.com>
2023-04-17 13:16:24 +0300

    Build wf (#1)
    * build workflow
    * release

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
-__version = "0.0.0.2"
+__version = "1.0.0"
 
 spec = {
     "name": "oc_ldap_user_locker",
     "version": __version,
     "license": "LGPLv2",
     "description": "LDAP user locker for use in scheduler",
+    "long_description": "",
+    "long_description_content_type": "text/plain",
     "packages": ["oc_ldap_user_locker"],
     "install_requires": [ 
-        'oc_ldap_client >= 1.0.0.0',
+        'oc_ldap_client >= 1.0.0',
       ],
     "python_requires": ">=3.6"
 }

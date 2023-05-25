1ed7bb60430eeb0dff13ecf144746bd92e36dbc3
Aleksey Knyazev <ctj_yebbs@inbox.ru>
2023-05-25 10:11:57 +0300

    mailer initialization tests

--- oc_ldap_user_locker/tests/test_mailer.py
+++ oc_ldap_user_locker/tests/test_mailer.py
@@ -23,10 +23,48 @@ class MailerTestCase(unittest.TestCase):
                 _mailer = LockMailer(_config, _config_pth)
 
     def test_check_config__partial_env_cnf(self):
-        pass
+        _config = {"user": "another_test_user"}
+        _config_pth = ""
+        _env_patch = {
+                "SMTP_URL": "smtp://smtp.example.com:25",
+                "SMTP_USER": "test_user"
+                }
+
+        with unittest.mock.patch.dict('os.environ', _env_patch):
+            with self.assertRaises(ValueError):
+                _mailer = LockMailer(_config, _config_pth)
 
     def test_check_config__all_env_cnf__mutual(self):
-        pass
+        _config = {"password": "test_user_password"}
+        _config_pth = ""
+        _env_patch = {
+                "SMTP_URL": "smtp://smtp.example.com:25",
+                "SMTP_USER": "test_user",
+                "MAIL_FROM": "test@example.com"}
+
+        with unittest.mock.patch.dict('os.environ', _env_patch):
+            _mailer = LockMailer(_config, _config_pth)
+            self.assertEqual(_mailer._config.get("password"), _config.get("password"))
+            self.assertEqual(_mailer._config.get("url"), _env_patch.get("SMTP_URL"))
+            self.assertEqual(_mailer._config.get("user"), _env_patch.get("SMTP_USER"))
+            self.assertEqual(_mailer._config.get("from"), _env_patch.get("MAIL_FROM"))
 
     def test_check_config__all_env_cnf__overwrite(self):
-        pass
+        _config = {
+                "url": "smtp://another.smtp.example.com:625",
+                "user": "another_test_user",
+                "password": "test_user_password",
+                "from": "another_test@example.com"}
+        _config_pth = ""
+        _env_patch = {
+                "SMTP_URL": "smtp://smtp.example.com:25",
+                "SMTP_USER": "test_user",
+                "SMTP_PASSWORD": "test_password",
+                "MAIL_FROM": "test@example.com"}
+
+        with unittest.mock.patch.dict('os.environ', _env_patch):
+            _mailer = LockMailer(_config, _config_pth)
+            self.assertEqual(_mailer._config.get("password"), _config.get("password"))
+            self.assertEqual(_mailer._config.get("url"), _config.get("url"))
+            self.assertEqual(_mailer._config.get("user"), _config.get("user"))
+            self.assertEqual(_mailer._config.get("from"), _config.get("from"))
